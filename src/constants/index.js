95da7a1dafc5c4cb900939c5ef1c4d0ae7821396
ash <a.motelchuk@gmail.com>
2025-07-18 00:54:15 +0300

    TEMPLATES_DIR

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,7 +10,7 @@ export const SMTP = {
     SMTP_FROM: "SMTP_FROM",
 };
 
-export const TEMPLATES_DIR = path.join(process.cwd(), "templates");
+export const TEMPLATES_DIR = path.join(process.cwd(), "src", "templates");
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), "temp");
 export const UPLOAD_DIR = path.join(process.cwd(), "uploads");
